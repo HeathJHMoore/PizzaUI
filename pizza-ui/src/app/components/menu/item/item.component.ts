a493c72a439787ad4a7400aba2d90604ca08834f
Itai-Kumengisa <imehku222@gmail.com>
2021-03-16 02:04:22 -0400

    pass an item object to each item component in the menu

--- pizza-ui/src/app/components/menu/item/item.component.ts
+++ pizza-ui/src/app/components/menu/item/item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../../../models/item';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+  @Input() foodItem: Item;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
